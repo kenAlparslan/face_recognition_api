b30ef5ac2c3b54d194174b886e9698807f652c99
Ken <47366236+kenAlparslan@users.noreply.github.com>
2020-02-19 06:25:07 -0500

    setting up endpoints

--- server.js
+++ server.js
@@ -1,11 +1,67 @@
 const express = require('express');
 
 const app = express();
+app.use(express.json());
+
+const database = {
+	users: [
+		{
+			id: '123',
+			name: 'john',
+			email: 'john@gmail.com',
+			password: 'cookies',
+			entries: 0,
+			joined: new Date()
+		},
+		{
+			id: '124',
+			name: 'sally',
+			email: 'sally@gmail',
+			password: 'milk',
+			entries: 0,
+			joined: new Date()
+		}
+	
+	]
+}
 
 app.get('/', (req, res) => {
-	res.send('this is working');
+	res.send(database.users);
+})
+
+app.post('/signin', (req, res) => {
+	if (req.body.email === database.users[0].email &&
+		 req.body.password === database.users[0].password) {
+		res.json('success');
+	} else {
+		res.status('404').json('error logging in');
+	}
+	
+})
+
+app.post('/register', (req, res) => {
+	const {email, name, password} = req.body;
+	database.users.push( {
+		id: '125',
+		name: name,
+		email: email,
+		password: password,
+		entries: 0,
+		joined: new Date()
+	})
+
+	res.json(database.users[database.users.length-1]);
 })
 
 app.listen(3000, () => {
 	console.log('app is running on port 3000');
 })
+
+/*
+/ --> res = this is working
+/signin --> POST = success/fail
+/register --> POST = user
+/profile/:userId --> GET = user
+/image --> PUT --> user(count)
+
+*/

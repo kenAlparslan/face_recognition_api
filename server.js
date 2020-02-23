2f205239513f3403b2458feb77c69ea192536aa3
Ken <47366236+kenAlparslan@users.noreply.github.com>
2020-02-23 18:09:32 -0500

    finished first version

--- server.js
+++ server.js
@@ -23,39 +23,26 @@ const db = knex({
 const app = express();
 app.use(express.json());
 app.use(cors());
-const database = {
-	users: [
-		{
-			id: '123',
-			name: 'john',
-			email: 'john@gmail.com',
-			password: 'cookies',
-			entries: 0,
-			joined: new Date()
-		},
-		{
-			id: '124',
-			name: 'sally',
-			email: 'sally@gmail',
-			password: 'milk',
-			entries: 0,
-			joined: new Date()
-		}
-	
-	]
-}
 
 app.get('/', (req, res) => {
 	res.json(database.users);
 })
 
 app.post('/signin', (req, res) => {
-	if (req.body.email === database.users[0].email &&
-		 req.body.password === database.users[0].password) {
-		res.json(database.users[0]);
-	} else {
-		res.status('404').json('error logging in');
-	}
+	db.select('email', 'hash').from('login')
+	.where('email', '=', req.body.email)
+	.then(data => {
+		const isValid = bcrypt.compareSync(req.body.password, data[0].hash);
+		if(isValid) {
+			return db.select('*').from('users').where('email', '=', req.body.email)
+			.then(user => {
+				res.json(user[0])
+			}).catch(err => res.status(400).json('Wrong credentials'))
+			
+		} else {
+			res.status(400).json('Wrong credentials');
+		}
+	}).catch(err => res.status(400).json('Unable to get user'))
 	
 })
 
